e3ad3c6fb4e08a5313a3a2fea41ce81bbc5b1c01
saifdigital12580-oss <saifdigital12580@gmail.com>
2026-08-02 20:23:40 +0500

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const authRouter = require("./Routes/authroutes")
+const authRouter = require("./Routes/authRoutes")
 const cors = require("cors");
 const OrderRouter = require("./Routes/OrderRoutes");
 const DashboardRouter = require("./Routes/DashbordRoute");
