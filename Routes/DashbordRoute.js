41369a6d94b1596cc7a1001ac1d2699942097b48
saifdigital12580-oss <saifdigital12580@gmail.com>
2026-08-04 16:40:41 +0500

    first commit

--- Routes/DashbordRoute.js
+++ Routes/DashbordRoute.js
@@ -1,15 +1,20 @@
 const express = require("express");
-
 const router = express.Router();
 
-
-const { MonthlySalesController,DashboardController ,RecentOrdersController,LatestCustomersController,LowStockController ,CategoryAnalyticsController,} = require("../Components/DashboardController");
-
+const {
+  DashboardController,
+  MonthlySalesController,
+  RecentOrdersController,
+  LatestCustomersController,
+  LowStockController,
+  CategoryAnalyticsController,
+} = require("../Components/DashboardController");
 
 router.get("/stats", DashboardController);
 router.get("/monthly-sales", MonthlySalesController);
 router.get("/recent-orders", RecentOrdersController);
-router.get("/latest-customers",LatestCustomersController);
-router.get(  "/low-stock",  LowStockController);
-router.get("/category-analytics",CategoryAnalyticsController);
+router.get("/latest-customers", LatestCustomersController);
+router.get("/low-stock", LowStockController);
+router.get("/category-analytics", CategoryAnalyticsController);
+
 module.exports = router;
